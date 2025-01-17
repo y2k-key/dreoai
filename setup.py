53aae3415422060662903e379fe63a36ce40acbb
Danny Brunelli <163336796+thecatprince@users.noreply.github.com>
2025-01-17 14:20:23 +0000

    Add files via upload

--- setup.py
+++ setup.py
@@ -16,17 +16,17 @@ requirements = []
 for reqs in install_requires:
     if "--" not in reqs and ":" not in reqs and "#" not in reqs:
         requirements.append(reqs)
-# Get version from version file in goai/version
-with open(os.path.join(this_directory, "goai/version"), encoding="utf-8") as f:
+# Get version from version file in vital/version
+with open(os.path.join(this_directory, "vital/version"), encoding="utf-8") as f:
     version = f.read().strip()
 
 setup(
-    name="goai",
+    name="vital",
     version=version,
     description="An Artificial Intelligence Automation Platform. AI Instruction management from various providers, has an adaptive memory, and a versatile plugin system with many commands including web browsing. Supports many AI providers and models and growing support every day.",
     long_description=long_description,
     long_description_content_type="text/markdown",  # This should match the format of your README
-    author="GoAI",
+    author="vital",
     packages=find_packages(),
     python_requires=">=3.10",
     install_requires=requirements,
--- start.py
+++ start.py
@@ -119,7 +119,7 @@ def check_prerequisites():
         print("Docker is not installed.")
         install = prompt_user("Would you like to install Docker? (y/n)", "y")
         if install.lower() != "y":
-            print("Docker is required to run goai. Exiting.")
+            print("Docker is required to run vital. Exiting.")
             sys.exit(1)
         if not install_docker():
             print("Failed to install Docker. Please install it manually and try again.")
@@ -129,7 +129,7 @@ def check_prerequisites():
         print("Docker Compose is not installed.")
         install = prompt_user("Would you like to install Docker Compose? (y/n)", "y")
         if install.lower() != "y":
-            print("Docker Compose is required to run goai. Exiting.")
+            print("Docker Compose is required to run vital. Exiting.")
             sys.exit(1)
         if not install_docker_compose():
             print(
@@ -170,29 +170,29 @@ def get_default_env_vars():
     workspace_folder = os.path.normpath(os.path.join(os.getcwd(), "WORKSPACE"))
     machine_tz = get_localzone()
     return {
-        "goai_API_KEY": "",
-        "goai_URI": "http://localhost:7437",
-        "goai_PORT": "7437",
-        "goai_INTERACTIVE_PORT": "3437",
-        "goai_AGENT": "goai",
-        "goai_BRANCH": "stable",
-        "goai_FILE_UPLOAD_ENABLED": "true",
-        "goai_VOICE_INPUT_ENABLED": "true",
-        "goai_FOOTER_MESSAGE": "Powered by goai",
-        "goai_REQUIRE_API_KEY": "false",
-        "goai_RLHF": "true",
-        "goai_SHOW_SELECTION": "conversation,agent",
-        "goai_SHOW_AGENT_BAR": "true",
-        "goai_SHOW_APP_BAR": "true",
-        "goai_CONVERSATION_MODE": "select",
-        "goai_SHOW_OVERRIDE_SWITCHES": "tts,websearch,analyze-user-input",
+        "vital_API_KEY": "",
+        "vital_URI": "http://localhost:7437",
+        "vital_PORT": "7437",
+        "vital_INTERACTIVE_PORT": "3437",
+        "vital_AGENT": "vital",
+        "vital_BRANCH": "stable",
+        "vital_FILE_UPLOAD_ENABLED": "true",
+        "vital_VOICE_INPUT_ENABLED": "true",
+        "vital_FOOTER_MESSAGE": "Powered by vital",
+        "vital_REQUIRE_API_KEY": "false",
+        "vital_RLHF": "true",
+        "vital_SHOW_SELECTION": "conversation,agent",
+        "vital_SHOW_AGENT_BAR": "true",
+        "vital_SHOW_APP_BAR": "true",
+        "vital_CONVERSATION_MODE": "select",
+        "vital_SHOW_OVERRIDE_SWITCHES": "tts,websearch,analyze-user-input",
         "ALLOWED_DOMAINS": "*",
-        "APP_DESCRIPTION": "A chat powered by goai.",
-        "APP_NAME": "goai Chat",
+        "APP_DESCRIPTION": "A chat powered by vital.",
+        "APP_NAME": "vital Chat",
         "APP_URI": "http://localhost:3437",
         "AUTH_WEB": "http://localhost:3437/user",
         "CREATE_AGENT_ON_REGISTER": "true",
-        "CREATE_goai_AGENT": "true",
+        "CREATE_vital_AGENT": "true",
         "DISABLED_PROVIDERS": "",
         "DISABLED_EXTENSIONS": "",
         "WORKING_DIRECTORY": workspace_folder.replace("\\", "/"),
@@ -201,11 +201,11 @@ def get_default_env_vars():
         "THEME_NAME": "doom",
         "ALLOW_EMAIL_SIGN_IN": "true",
         "DATABASE_TYPE": "sqlite",
-        "DATABASE_NAME": "models/goai",
+        "DATABASE_NAME": "models/vital",
         "LOG_LEVEL": "INFO",
         "LOG_FORMAT": "%(asctime)s | %(levelname)s | %(message)s",
         "UVICORN_WORKERS": "10",
-        "goai_AUTO_UPDATE": "true",
+        "vital_AUTO_UPDATE": "true",
         "EZLOCALAI_URI": f"http://{get_local_ip()}:8091/v1/",
         "DEFAULT_MODEL": "QuantFactory/dolphin-2.9.2-qwen2-7b-GGUF",
         "VISION_MODEL": "deepseek-ai/deepseek-vl-1.3b-chat",
@@ -214,9 +214,9 @@ def get_default_env_vars():
         "GPU_LAYERS": "0",
         "WITH_EZLOCALAI": "false",
         "REGISTRATION_DISABLED": "false",
-        "goai_ALLOW_MESSAGE_EDITING": "true",
-        "goai_ALLOW_MESSAGE_DELETION": "true",
-        "goai_SHOW_CHAT_THEME_TOGGLES": "",
+        "vital_ALLOW_MESSAGE_EDITING": "true",
+        "vital_ALLOW_MESSAGE_DELETION": "true",
+        "vital_SHOW_CHAT_THEME_TOGGLES": "",
         "LOG_VERBOSITY_SERVER": "3",
         "AOL_CLIENT_ID": "",
         "AOL_CLIENT_SECRET": "",
@@ -250,9 +250,9 @@ def set_environment(env_updates=None):
         for key, value in env_updates.items():
             if key in env_vars:
                 env_vars[key] = value
-    # Ensure goai_API_KEY is set
-    if env_vars["goai_API_KEY"] == "":
-        env_vars["goai_API_KEY"] = "".join(
+    # Ensure vital_API_KEY is set
+    if env_vars["vital_API_KEY"] == "":
+        env_vars["vital_API_KEY"] = "".join(
             random.choice(
                 "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
             )
@@ -268,13 +268,13 @@ def set_environment(env_updates=None):
         print("Starting ezLocalai, this can take several minutes...")
         start_ezlocalai()
     dockerfile = "docker-compose.yml"
-    if env_vars["goai_BRANCH"] != "stable":
+    if env_vars["vital_BRANCH"] != "stable":
         dockerfile = "docker-compose-dev.yml"
-    if env_vars["goai_BRANCH"] != "stable":
+    if env_vars["vital_BRANCH"] != "stable":
         dockerfile = "docker-compose-dev.yml"
     else:
         dockerfile = "docker-compose.yml"
-    if str(env_vars["goai_AUTO_UPDATE"]).lower() == "true":
+    if str(env_vars["vital_AUTO_UPDATE"]).lower() == "true":
         command = f"docker compose -f {dockerfile} stop && docker compose -f {dockerfile} pull && docker compose -f {dockerfile} up -d"
     else:
         command = f"docker compose -f {dockerfile} stop && docker compose -f {dockerfile} up -d"
@@ -282,9 +282,9 @@ def set_environment(env_updates=None):
     try:
         run_shell_command(command)
     except KeyboardInterrupt:
-        print("\nStopping goai containers...")
+        print("\nStopping vital containers...")
         run_shell_command(f"docker compose -f {dockerfile} stop")
-        print("goai containers stopped.")
+        print("vital containers stopped.")
     except subprocess.CalledProcessError as e:
         print(f"An error occurred: {e}")
     return env_vars
@@ -381,7 +381,7 @@ def start_ezlocalai():
 
 if __name__ == "__main__":
     check_prerequisites()
-    parser = argparse.ArgumentParser(description="goai Environment Setup")
+    parser = argparse.ArgumentParser(description="vital Environment Setup")
     # Add arguments for each environment variable
     for key, value in get_default_env_vars().items():
         parser.add_argument(
@@ -396,18 +396,18 @@ if __name__ == "__main__":
     arg_dict = {k: v for k, v in vars(args).items() if v is not None}
     # Convert hyphenated arg names back to underscore format
     env_updates = {k.upper().replace("-", "_"): v for k, v in arg_dict.items()}
-    # Check if .env file exists and if goai_AUTO_UPDATE is not set via command line
-    if not os.path.exists(".env") and "goai_AUTO_UPDATE" not in env_updates:
+    # Check if .env file exists and if vital_AUTO_UPDATE is not set via command line
+    if not os.path.exists(".env") and "vital_AUTO_UPDATE" not in env_updates:
         auto_update = prompt_user(
-            "Would you like goai to auto update when this script is run in the future? (Y for yes, N for no)",
+            "Would you like vital to auto update when this script is run in the future? (Y for yes, N for no)",
             "y",
         )
         if auto_update.lower() == "y" or auto_update.lower() == "yes":
             auto_update = "true"
         else:
             auto_update = "false"
-        env_updates["goai_AUTO_UPDATE"] = auto_update
+        env_updates["vital_AUTO_UPDATE"] = auto_update
     # Apply updates and restart server
-    print("Please wait while goai is starting, this can take several minutes...")
+    print("Please wait while vital is starting, this can take several minutes...")
     set_environment(env_updates=env_updates)
 
